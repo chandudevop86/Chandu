ac2b3de61b63c5a45fadfd61f298f3d8d1a83a27
puttaramesh171@gmail.com <puttaramesh171@gmail.com>
2026-05-02 00:10:41 +0530

    env file

--- app/vinayak/auth/utils.py
+++ app/vinayak/auth/utils.py
@@ -1,6 +1,9 @@
 from passlib.context import CryptContext
 
-pwd_context = CryptContext(schemes=["bcrypt"], deprecated="auto")
+pwd_context = CryptContext(
+    schemes=["bcrypt", "pbkdf2_sha256"],  # ✅ ADD THIS
+    deprecated="auto",
+)
 
 def verify_password(plain_password: str, hashed_password: str) -> bool:
     return pwd_context.verify(plain_password, hashed_password)
